08c7422fcdb2d033436ccc85c5027aff940063db
sonia <ssontshatsha@gmail.com>
2020-03-23 14:26:58 +0200

    udpate

--- src/test/java/ac/za/cput/AppTest.java
+++ src/test/java/ac/za/cput/AppTest.java
@@ -1,20 +1,35 @@
 package ac.za.cput;
 
+import static org.junit.Assert.assertArrayEquals;
 import static org.junit.Assert.assertTrue;
 
+import org.junit.Assert;
+import org.junit.Before;
 import org.junit.Test;
+
+import java.util.*;
 
 /**
  * Unit test for simple App.
  */
 public class AppTest 
 {
-    /**
-     * Rigorous Test :-)
-     */
-    @Test
-    public void shouldAnswerWithTrue()
-    {
-        assertTrue( true );
+   App app;
+   Map<Integer, String> map;
+   Set<String> set;
+   List<String> list;
+
+    @Before
+    public void setUp()  {
+        app = new App();
+        map = new HashMap<>();
+        set = new TreeSet<>();
+        list = new ArrayList<>();
     }
+
+    @Test
+    public void testMap(){
+        Assert.assertTrue("Map is matching the values", map.equals(map));
+    }
+
 }
